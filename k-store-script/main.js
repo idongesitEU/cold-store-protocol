c7990d565cdebbec48ff567da83817479ceaacf3
Idongesit Udosen <IdongesitEU@outlook.com>
2024-04-30 14:20:08 +0100

    Update and rename playground.js to main.js

---
+++ k-store-script/main.js
@@ -0,0 +1,6 @@
+const {
+	buildPassword
+} = require("./password-builder");
+const BASE_KEY_HASH = '' + process.env.BASE_KEY_HASH;
+const SITE_NAME = NAME_OF_SITE ';
+buildPassword(process.env.BASE, process.env.PADDING, BASE_KEY_HASH, SITE_NAME);
