b8cd99398ffad7f81eb8ecd864ec488389a2ebdc
Idongesit Udosen <IdongesitEU@outlook.com>
2024-04-30 14:28:06 +0100

    Update main.js

--- k-store-script/main.js
+++ k-store-script/main.js
@@ -2,5 +2,5 @@ const {
 	buildPassword
 } = require("./password-builder");
 const BASE_KEY_HASH = '' + process.env.BASE_KEY_HASH;
-const SITE_NAME = NAME_OF_SITE ';
+const SITE_NAME = NAME_OF_SITE;
 buildPassword(process.env.BASE, process.env.PADDING, BASE_KEY_HASH, SITE_NAME);
